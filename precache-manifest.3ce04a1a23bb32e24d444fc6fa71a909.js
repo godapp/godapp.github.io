8087398fde7668feda3fe84dd98d13c763104125
Shane <shane@yztz.cn>
2018-11-20 20:18:47 +0800

    initial

---
+++ precache-manifest.3ce04a1a23bb32e24d444fc6fa71a909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "05cb0159426df81252cc",
+    "url": "/js/chunk-vendors.fc5d4896.js"
+  },
+  {
+    "revision": "344a2ffe6115dde539da",
+    "url": "/js/app.59906136.js"
+  },
+  {
+    "revision": "0d2e527f1b2ea6c5785c2442e108ed47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "344a2ffe6115dde539da",
+    "url": "/css/app.72d3dadb.css"
+  }
+];
