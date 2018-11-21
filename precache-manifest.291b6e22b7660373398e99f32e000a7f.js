0df15f6e2d4d3883a198871c41cb7f41111f6ef3
Shane <shane@yztz.cn>
2018-11-21 09:23:17 +0800

    change cache

--- precache-manifest.291b6e22b7660373398e99f32e000a7f.js
+++ precache-manifest.291b6e22b7660373398e99f32e000a7f.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.fc5d4896.js"
   },
   {
-    "revision": "344a2ffe6115dde539da",
-    "url": "/js/app.59906136.js"
+    "revision": "2e5d45462dd2a3db72cf",
+    "url": "/js/app.24c4e7c4.js"
   },
   {
-    "revision": "0d2e527f1b2ea6c5785c2442e108ed47",
+    "revision": "133f7b8c203ed6ac7c21a6b9dcb6dab9",
     "url": "/index.html"
   },
   {
-    "revision": "344a2ffe6115dde539da",
+    "revision": "2e5d45462dd2a3db72cf",
     "url": "/css/app.72d3dadb.css"
   }
 ];
