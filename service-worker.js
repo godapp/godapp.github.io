ebf2105054e5c72b91e6e3f23e536b3bf8c049a9
michael <zhangxiaocong_2017@126.com>
2019-01-18 17:13:36 +0800

    修改字体

--- service-worker.js
+++ service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "/precache-manifest.cf6419ebecebb652206e5f0a65a940d9.js"
-=======
-  "/precache-manifest.4b9130ec24c4d7c94ef1fee7a0d64e55.js"
->>>>>>> 9603944dc460066d1d56bce4f038917c747dc747
+  "/precache-manifest.6c9640544cf3e86d1013a1e18fc1e74f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "godapp"});
