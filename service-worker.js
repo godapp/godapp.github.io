558729a8594ff9ab4e4b6cdb531ef799d61535d6
michael <zhangxiaocong_2017@126.com>
2019-01-31 18:24:17 +0800

    clasic baccarat

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dde7617b7fcc3644008733dd20b470ac.js"
+  "/precache-manifest.f7844f14d2abffe0aceac00137738bf4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dapp365"});
