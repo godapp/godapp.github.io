8c0327f838aa49d592829087d4d1c551b1e6bda6
Shane <shane@yztz.cn>
2019-03-13 14:11:39 +0800

    波场百家乐

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6965828372b9025abe8dc1e31af50168.js"
+  "/precache-manifest.fd1d68471e8ecbefd5d96851be182dcf.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dapp365"});
